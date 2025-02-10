8a8b20e1f223ffd8bb0356cf8ae6c65e5a514bd2
su apt install wayz <wayzzy59@gmail.com>
2025-02-10 16:11:21 +0100

    Add Header component and update App.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <main class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-[1rem]">
+  <Header />
+  <main class="w-full h-full min-h-[calc(100vh-64px)] mt-[64px] flex flex-col items-center justify-center bg-gray-100 p-[1rem]">
     <router-view />
   </main>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import Header from '@/components/Header.vue';
 </script>
